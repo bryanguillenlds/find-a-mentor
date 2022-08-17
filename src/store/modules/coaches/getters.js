ca1d075a5791923e7b4807288482c2b474934b25
bryan <bryanguillenlds@gmail.com>
2022-08-16 20:55:09 -0600

    Created mutation and action to register a coach. Added getter to check if user is already a coach. Created component for registering a coach wit validation. Added methods to save the data and added a state (hardcoded for the moment) for keeping track of current user's id.

--- src/store/modules/coaches/getters.js
+++ src/store/modules/coaches/getters.js
@@ -1,4 +1,10 @@
 export default {
   coaches: state => state.coaches,
-  hasCoaches: state => state.coaches && state.coaches.length > 0
+  hasCoaches: state => state.coaches && state.coaches.length > 0,
+  isCoach: (state, getters, rootState, rootGetters) => {
+    const coaches = getters.coaches;
+    const userId = rootGetters.userId;
+
+    return coaches.some(coach => coach.id === userId);
+  }
 };
